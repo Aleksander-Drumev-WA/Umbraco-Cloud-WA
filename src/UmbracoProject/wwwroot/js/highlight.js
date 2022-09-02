893e45e889764225aeb07a5c10ed3ecd1a1bc1f2
Aleksander-Drumev-WA <Aleksander.Drumev@waveaccess.global>
2022-09-02 15:23:57 +0300

    Issue #5 solved

--- src/UmbracoProject/wwwroot/js/highlight.js
+++ src/UmbracoProject/wwwroot/js/highlight.js
@@ -8,20 +8,17 @@ for (var i = 0; i < document.links.length; i++) {
 // Change language buttons background color
 var bgBtn = document.getElementById('bg-btn');
 var enBtn = document.getElementById('en-btn');
-if (!getCurrentURL().includes('/blog-posts-container/') && !getCurrentURL().includes('/categories/')) {
 
-    if (document.links[current].href.includes('/bg/')) {
-        bgBtn.style.backgroundColor = 'black';
-        bgBtn.style.color = 'white';
-    }
-    else {
-        enBtn.style.backgroundColor = 'black';
-        enBtn.style.color = 'white';
-    }
+if (document.links[current].href.includes('/bg/')) {
+    bgBtn.style.backgroundColor = 'black';
+    bgBtn.style.color = 'white';
+
+    bgBtn.disabled = true;
 }
 else {
-    // Disable if url is in article
-    bgBtn.disabled = true;
+    enBtn.style.backgroundColor = 'black';
+    enBtn.style.color = 'white';
+
     enBtn.disabled = true;
 }
 
@@ -30,29 +27,4 @@ else {
 
 if (document.links[current].className !== "site-name") {
     document.links[current].className = 'current';
-}
-
-
-// Change language dynamically
-
-function changeLanguage(e) {
-    let url = getCurrentURL();
-
-    if (!url.includes('/blog-posts-container/')) {
-
-        if (e.target.id === 'en-btn' && url.includes('/bg/')) {
-            url = url.replace('/bg/', '/en/');
-        }
-        else if (e.target.id === "bg-btn" && url.includes('/en/')) {
-            url = url.replace('/en/', '/bg/');
-        }
-
-        window.location.href = url;
-    }
-
-
-}
-
-function getCurrentURL() {
-    return window.location.href
 }
